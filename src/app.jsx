f343c99fb22a3d1b8e9fd380427c5aae146098e0
matthewdking <matthew.d.king@outlook.com>
2017-10-07 11:27:35 +0300

    change App to class component

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => <h1 className="app"> Hey I&lsquo;m React&lsquo;s setup </h1>;
+// const App = () => <h1 className="app"> Hey I&lsquo;m React&lsquo;s setup </h1>;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hello: 'hi'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="app"> Hey I&lsquo;m React&lsquo;s setup </h1>
+        <h1>{ this.state.hello }</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
